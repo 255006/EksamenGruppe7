ca953cbcac6f2210047cb7dd14428361edcf151c
Oliwier Jagus <255006@usn.no>
2024-05-21 15:03:15 +0200

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace EksamenGruppe7.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
