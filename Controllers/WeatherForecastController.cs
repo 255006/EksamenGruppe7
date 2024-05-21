ad230fe92a87ae1809982144a88bb47c1a27bdc8
Oliwier Jagus <255006@usn.no>
2024-05-21 15:13:31 +0200

    Deloppgave 2 ferdig.

---
+++ Controllers/WeatherForecastController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EksamenGruppe7.Controllers
+{
+    public class WeatherForecastController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
