ee715356749f3957df3400a65d5911360d205f78
Oliwier Jagus <255006@usn.no>
2024-05-21 15:19:25 +0200

    Lagde modell for weatherforecast.

---
+++ Models/WeatherForecast.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace EksamenGruppe7.Models
+{
+    public class WeatherForecast
+    {
+        public Guid Id { get; set; } // UUID for primary key
+        public DateTime Date { get; set; }
+        public double Temperature { get; set; }
+        public string Comment { get; set; }
+    }
+}
+
